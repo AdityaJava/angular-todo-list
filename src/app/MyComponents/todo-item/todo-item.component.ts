fe12535b3ae76380e733502655e18da70442ae25
AdityaDudhal <a.dudhal@maxxton.com>
2025-06-06 10:57:45 +0530

    feat: enhance TodoItem component with delete functionality and update layout

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Todo } from '../../Todo';
 
 @Component({
@@ -9,10 +9,22 @@ import { Todo } from '../../Todo';
 })
 export class TodoItemComponent {
 
+  //This is an input property that will receive the todo item from the parent component
+  //The parent component will pass the todo item to this component
   @Input()
   todo! : Todo;
 
+  //This is an event emitter that will emit the todo item when the delete button is clicked
+  //This is used to communicate with the parent component
+  //The parent component will listen to this event and will delete the todo item from the list
+  @Output()
+  todoDelete: EventEmitter<Todo> = new EventEmitter<Todo>();
+
   constructor(){
 
+  }
+
+  onDelete() {
+    console.log("Delete todo with id: " + this.todo.id);      
   }
 }
